c7c0872e6f9144c61e771146cc90aaf0985f3eab
AbdullaKhedr <60980677+AbdullaKhedr@users.noreply.github.com>
2021-10-24 18:36:37 +0300

    Camera & Viewing

---
+++ CG_F21_L02/09_Transformation/src/Camera.cpp
@@ -0,0 +1,72 @@
+#include "Camera.h"
+
+#include"glm/gtc/matrix_transform.hpp"
+
+const float FOV = 45.0f;
+
+Camera::Camera()
+	:mPosition(glm::vec3(0.0f, 0.0f, 0.0f)),
+	mTargetPos(glm::vec3(0.0f, 0.0f, 0.0f)),
+	mUp(glm::vec3(0.0f, 0.0f, 0.0f)),
+	mRight(glm::vec3(0.0f, 0.0f, 0.0f)),
+	worldUp(glm::vec3(0.0f, 0.0f, 0.0f)),
+	mYaw(glm::pi<float>()),
+	mFOV(FOV)
+{}
+
+glm::mat4 Camera::getViewMatrix() const
+{
+	return glm::lookAt(mPosition, mTargetPos, mUp);
+}
+
+const glm::vec3& Camera::getLook() const
+{
+	return mLook;
+}
+const glm::vec3& Camera::getRight() const
+{
+	return mRight;
+}
+const glm::vec3& Camera::getUp() const
+{
+	return mUp;
+}
+
+FBSCamera::FBSCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), float yaw = glm::pi<float>(), float pitch = 0.0f)
+{
+	mPosition = position;
+	mYaw = yaw;
+	mPitch = pitch;
+}
+FBSCamera::FBSCamera(glm::vec3 position, glm::vec3 target)
+{
+	mPosition = position;
+	mTargetPos = target;
+
+	glm::vec3 lookDir = position - target;
+
+	mYaw = atan2(lookDir.x, lookDir.z) + glm::pi<float>();
+	mPitch = -atan2(lookDir.y, sqrt(pow(lookDir.x, 2) + pow(lookDir.z, 2)));
+}
+
+void FBSCamera::setPosition(const glm::vec3& position)
+{
+	mPosition = position;
+}
+void FBSCamera::rotate(float yaw, float pitch)
+{
+	mYaw += glm::radians(yaw);
+	mPitch += glm::radians(pitch);
+
+	// add constrains on the values to be on the range
+	mPitch = glm::clamp(mPitch, -glm::pi<float>() / 2.0f + 0.1f, glm::pi<float>() / 2.0f - 0.1f);
+	if (mYaw > glm::two_pi<float>())
+		mYaw -= glm::two_pi<float>();
+	if (mYaw < 0.0)
+		mYaw += glm::two_pi<float>();
+
+}
+void FBSCamera::move(const glm::vec3& offset)
+{
+
+}
---
+++ CG_F21_L02/09_Transformation/src/Camera.h
@@ -0,0 +1,63 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "glm/glm.hpp"
+#include"glm/gtc/constants.hpp"
+
+class Camera
+{
+public:
+	glm::mat4 getViewMatrix() const;
+
+	virtual void setPosition(const glm::vec3& position);
+	virtual void rotate(float yaw, float pitch); // not doing rolling!!
+	virtual void move(const glm::vec3& offset);
+
+	const glm::vec3& getLook() const;
+	const glm::vec3& getRight() const;
+	const glm::vec3& getUp() const;
+
+	float getFOV() const { return mFOV; }
+	void setFOV(float fov) { mFOV = fov; }
+
+protected:
+
+	Camera();
+
+	virtual void updateCameraVectors();
+
+	glm::vec3 mPosition;
+	glm::vec3 mTargetPos;
+	glm::vec3 mLook; //fwd vector
+	glm::vec3 mUp; // camera up
+	glm::vec3 mRight;
+	const glm::vec3 worldUp; // world up
+
+	// Camera parameters
+	float mYaw;
+	float mPitch;
+	float mFOV;
+
+};
+
+// implementation of FPS Camera
+
+class FBSCamera : public Camera
+{
+public:
+	FBSCamera(glm::vec3 position, float yaw, float pitch);
+	FBSCamera(glm::vec3 position, glm::vec3 target);
+
+	virtual void setPosition(const glm::vec3& position);
+	virtual void rotate(float yaw, float pitch);
+	virtual void move(const glm::vec3& offset);
+
+private:
+	void updateCameraVectors();
+
+};
+
+#endif
+
+
+
